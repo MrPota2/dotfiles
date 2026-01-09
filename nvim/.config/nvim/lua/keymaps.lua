117573a160b44d5c1e580b9578d61356d98fcb3d
Adrian Milvang <adrian.milvang@gmail.com>
2026-01-09 14:19:46 +0100

    fix some formatting

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -66,9 +66,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
-
-vim.api.nvim_create_autocmd('BufWritePre', {
-  callback = function()
-    vim.lsp.buf.format { async = false }
-  end,
-})
--- nvim/.config/nvim/lua/kickstart/plugins/fuzzy-finder.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/fuzzy-finder.lua
@@ -75,18 +75,26 @@ return {
       require('telescope').load_extension 'luasnip'
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
+      vim.keymap.set('n', '<leader>sf', function()
+        local ok = pcall(builtin.git_files, { show_untracked = true })
+        if not ok then
+          builtin.find_files()
+        end
+      end, { desc = '[S]earch [F]iles' })
+
+      vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>s.', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-      vim.keymap.set('n', '<leader>sgb', builtin.git_branches, { desc = '[S]earch [G]it branches ("." for repeat)' })
+      vim.keymap.set('n', '<leader>svb', builtin.git_branches, { desc = '[S]earch [V]ersion control git [B]ranches' })
+      vim.keymap.set('n', '<leader>svf', builtin.git_files, { desc = '[S]earch [V]ersion control git [F]iles' })
+      vim.keymap.set('n', '<leader>svs', builtin.git_status, { desc = '[S]earch [V]ersion control git [S]tatus' })
       vim.keymap.set('n', '<leader>sp', require('telescope').extensions.luasnip.luasnip, { desc = '[S]earch sni[P]pets' })
 
       -- Slightly advanced example of overriding default behavior and theme
