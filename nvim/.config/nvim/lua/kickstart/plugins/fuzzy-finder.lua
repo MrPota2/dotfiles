b20a225a009de59d26da85efab62257ed778f0af
Adrian Milvang <adrian.milvang@gmail.com>
2026-01-20 18:32:17 +0100

    Add luasnip

--- nvim/.config/nvim/lua/kickstart/plugins/fuzzy-finder.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/fuzzy-finder.lua
@@ -21,7 +21,7 @@ return {
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-      'benfowler/telescope-luasnip',
+      'benfowler/telescope-luasnip.nvim',
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
