65ac49356418c32e08e275aa1ada081460bc8f07
Adrian Milvang <adrian.milvang@gmail.com>
2026-03-04 08:29:29 +0100

    cachyOS

--- zsh/.config/zshrc/completions.zsh
+++ zsh/.config/zshrc/completions.zsh
@@ -1,7 +1,7 @@
 # Added by deno for some reason
 if [[ ":$FPATH:" != *":/home/adrian/.zsh/completions:"* ]]; then export FPATH="/home/adrian/.zsh/completions:$FPATH"; fi
 
-COMPLETIONS_STR=$(fdfind --glob '*.zsh' ~/.config/zshrc/completions)
+COMPLETIONS_STR=$(fd --glob '*.zsh' ~/.config/zshrc/completions)
 COMPLETIONS=($(echo $COMPLETIONS_STR | tr '\n' ' '))
 
 for COMPLETION in $COMPLETIONS; do
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -45,3 +45,7 @@ else
 fi
 
 eval "$(zoxide init zsh)" # Must be ran after compinit so is added here
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
