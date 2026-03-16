a9924ed145921041823278a544030b70bc686dea
Adrian Milvang <adrian.milvang@gmail.com>
2026-03-16 16:53:19 +0100

    kde stuff

--- nvim/.config/nvim/lua/lazy-plugins.lua
+++ nvim/.config/nvim/lua/lazy-plugins.lua
@@ -42,6 +42,18 @@ require('lazy').setup({
     },
   },
   {
+    'selimacerbas/markdown-preview.nvim',
+    dependencies = { 'selimacerbas/live-server.nvim' },
+    config = function()
+      require('markdown_preview').setup {
+        -- all optional; sane defaults shown
+        port = 8421,
+        open_browser = true,
+        debounce_ms = 300,
+      }
+    end,
+  },
+  {
     'zongben/dbout.nvim',
     build = 'npm install',
     lazy = 'VeryLazy',
