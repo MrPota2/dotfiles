ce22c94b684ff5806f9c13a307144e6805d28a1b
Adrian Milvang <adrian.milvang@gmail.com>
2026-05-05 07:06:17 +0200

    aerial

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -3,6 +3,8 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.opt.diffopt:append { 'iwhiteeol' }
+
 vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.o.foldlevel = 99
