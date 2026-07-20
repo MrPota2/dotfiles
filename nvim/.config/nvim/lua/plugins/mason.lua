6cd8bb413f5e3458416465830aa792cfb4fe024b
Adrian Milvang <adrian.milvang@gmail.com>
2026-07-20 12:40:54 +0200

    Add ensureinstalled for lsps in nvim

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,15 @@
 ---@type LazySpec
 return {
   {
-    'mason-org/mason.nvim',
-    opts = {},
-  },
+    'mason-org/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = { "lua_ls", "rust_analyzer", "oxfmt", "oxlint", "typescript-language-server" }
+    },
+    dependencies = {
+      {
+        'mason-org/mason.nvim',
+        opts = {},
+      },
+    }
+  }
 }
